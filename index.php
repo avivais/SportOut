19d0c4d4886a6b2afbfa52ebfcf0bf699c544c27
Avi Vaisenberger <avi@mymedia.club>
2015-12-18 14:45:57 +0200

    Minify css

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="css/pickadate/default.date.css">
     <link rel="stylesheet" href="css/styles.css">
 
-    <title>Monday BBall</title>
+    <title>SportOut</title>
 </head>
 <body>
     <nav class="navbar navbar-default navbar-fixed-top">
@@ -24,10 +24,8 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Rieglers</a>
+                <a class="navbar-brand" href="#">Riegler 15/16</a>
             </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="topNavBar">
                 <ul class="nav navbar-nav">
                     <li class="dropdown">
@@ -48,11 +46,14 @@
             </div>
         </div>
     </nav>
-    <div class="container-fluid game-container" style="display: none">
+    <div class="container-fluid game-container">
         <div id="gameInfo">
             <div id="startTime">
                 <div class="lead text-center">
                     <span class="label label-default"><small>7/2 21:30</small></span>
+                </div>
+                <div class="lead text-center">
+                    <span class="label label-success"><small>7m</small></span>
                 </div>
             </div>
             <div id="finalScore" class="row">
@@ -125,57 +126,53 @@
                 <div class="row team-name left-color">The Lefts</div>
                 <div class="row headers">
                     <div class="col-md-offset-4 col-md-1 col-xs-2 total text-center">Tot</div>
-                    <div class="col-md-2 col-xs-2 2pt-nom text-center">2pt</div>
-                    <div class="col-md-1 col-xs-2 2pt-pct text-center">%</div>
-                    <div class="col-md-2 col-xs-2 3pt-nom text-center">3pt</div>
-                    <div class="col-md-1 col-xs-2 3pt-pct text-center">%</div>
+                    <div class="col-md-3 col-xs-4 2pt text-center">2pt M / A %</div>
+                    <div class="col-md-3 col-xs-4 3pt text-center">3pt</div>
                     <div class="col-md-1 col-xs-2 fouls text-center">Fouls</div>
+                </div>
+                <div class="row player-row">
+                    <div class="col-md-4 player-name"><strong>Avi Vaisenberger</strong></div>
+                    <div class="col-md-1 col-xs-2 total text-center">9</div>
+                    <div class="col-md-3 col-xs-4 2pt-nom text-center">0 / 0</div>
+                    <div class="col-md-3 col-xs-4 3pt-nom text-center">3 / 5 <small><strong>60%</strong></small></div>
+                    <div class="col-md-1 col-xs-2 fouls text-center">3</div>
+                </div>
+                <div class="row player-row">
+                    <div class="col-md-4 player-name"><strong>Yoni Reshef</strong></div>
+                    <div class="col-md-1 col-xs-2 total text-center">9</div>
+                    <div class="col-md-3 col-xs-4 2pt-nom text-center">0 / 0</div>
+                    <div class="col-md-3 col-xs-4 3pt-nom text-center">3 / 5 <small><strong>60%</strong></small></div>
+                    <div class="col-md-1 col-xs-2 fouls text-center">3</div>
+                </div>
+                <div class="row player-row">
+                    <div class="col-md-4 player-name"><strong>Eitan Torf</strong></div>
+                    <div class="col-md-1 col-xs-2 total text-center">9</div>
+                    <div class="col-md-3 col-xs-4 2pt-nom text-center">0 / 0</div>
+                    <div class="col-md-3 col-xs-4 3pt-nom text-center">3 / 5 <small><strong>60%</strong></small></div>
+                    <div class="col-md-1 col-xs-2 fouls text-center">3</div>
+                </div>
+                <div class="row player-row">
+                    <div class="col-md-4 player-name"><strong>Maor Moshe</strong></div>
+                    <div class="col-md-1 col-xs-2 total text-center">9</div>
+                    <div class="col-md-3 col-xs-4 2pt-nom text-center">0 / 0</div>
+                    <div class="col-md-3 col-xs-4 3pt-nom text-center">3 / 5 <small><strong>60%</strong></small></div>
+                    <div class="col-md-1 col-xs-2 fouls text-center">3</div>
                 </div>
                 <div class="row player-row">
                     <div class="col-md-4 player-name"><strong>Roy Sharf</strong></div>
                     <div class="col-md-1 col-xs-2 total text-center">9</div>
-                    <div class="col-md-2 col-xs-2 2pt-nom text-center">0 / 0</div>
-                    <div class="col-md-1 col-xs-2 2pt-pct text-center">-</div>
-                    <div class="col-md-2 col-xs-2 3pt-nom text-center">3 / 5</div>
-                    <div class="col-md-1 col-xs-2 3pt-pct text-center">60%</div>
-                    <div class="col-md-1 col-xs-2 fouls text-center">3</div>
-                </div>
-                <div class="row player-row">
-                    <div class="col-md-4 player-name"><strong>Avi Vaisenberger</strong></div>
-                    <div class="col-md-1 col-xs-2 total text-center">11</div>
-                    <div class="col-md-2 col-xs-2 2pt-nom text-center">1 / 4</div>
-                    <div class="col-md-1 col-xs-2 2pt-pct text-center">25%</div>
-                    <div class="col-md-2 col-xs-2 3pt-nom text-center">3 / 6</div>
-                    <div class="col-md-1 col-xs-2 3pt-pct text-center">50%</div>
+                    <div class="col-md-3 col-xs-4 2pt-nom text-center">0 / 0</div>
+                    <div class="col-md-3 col-xs-4 3pt-nom text-center">3 / 5 <small><strong>60%</strong></small></div>
                     <div class="col-md-1 col-xs-2 fouls text-center">3</div>
                 </div>
             </div>
             <div id="rightTeam" class="col-md-6">
                 <div class="row team-name right-color">The Rights</div>
-                <div class="row headers">
-                    <div class="col-md-offset-4 col-md-1 col-xs-2 total text-center">Tot</div>
-                    <div class="col-md-2 col-xs-2 2pt-nom text-center">2pt</div>
-                    <div class="col-md-1 col-xs-2 2pt-pct text-center">%</div>
-                    <div class="col-md-2 col-xs-2 3pt-nom text-center">3pt</div>
-                    <div class="col-md-1 col-xs-2 3pt-pct text-center">%</div>
-                    <div class="col-md-1 col-xs-2 fouls text-center">Fouls</div>
-                </div>
                 <div class="row player-row">
                     <div class="col-md-4 player-name"><strong>Roy Sharf</strong></div>
                     <div class="col-md-1 col-xs-2 total text-center">9</div>
-                    <div class="col-md-2 col-xs-2 2pt-nom text-center">0 / 0</div>
-                    <div class="col-md-1 col-xs-2 2pt-pct text-center">-</div>
-                    <div class="col-md-2 col-xs-2 3pt-nom text-center">3 / 5</div>
-                    <div class="col-md-1 col-xs-2 3pt-pct text-center">60%</div>
-                    <div class="col-md-1 col-xs-2 fouls text-center">3</div>
-                </div>
-                <div class="row player-row">
-                    <div class="col-md-4 player-name"><strong>Avi Vaisenberger</strong></div>
-                    <div class="col-md-1 col-xs-2 total text-center">11</div>
-                    <div class="col-md-2 col-xs-2 2pt-nom text-center">1 / 4</div>
-                    <div class="col-md-1 col-xs-2 2pt-pct text-center">25%</div>
-                    <div class="col-md-2 col-xs-2 3pt-nom text-center">3 / 6</div>
-                    <div class="col-md-1 col-xs-2 3pt-pct text-center">50%</div>
+                    <div class="col-md-3 col-xs-4 2pt-nom text-center">0 / 0</div>
+                    <div class="col-md-3 col-xs-4 3pt-nom text-center">3 / 5 <small><strong>60%</strong></small></div>
                     <div class="col-md-1 col-xs-2 fouls text-center">3</div>
                 </div>
             </div>
